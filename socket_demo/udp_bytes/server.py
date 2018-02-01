50760e99749a1f65b6d1b29bd372f90749f9ebad
robin <wming521314@gmail.com>
2018-02-01 18:12:30 +0800

    add some annotations

--- socket_demo/udp_bytes/server.py
+++ socket_demo/udp_bytes/server.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     print '...waiting for message..'
     while True:
         data, address = sock.recvfrom(24)
-        print(address)
-        print(type(data)) #<type 'str'>
-        print(len(data))
-        print(data)
-        print(binascii.hexlify(data)) #00c029e9785991b13173a11a30ebbe198a61f5292e4ca433
+        print(address) # ('127.0.0.1', 35550)
+        print(type(data)) # <type 'str'>
+        print(len(data)) # 24 12
+        print(data) # xxxxx hello server
+        print(binascii.hexlify(data)) #00c029e9785991b13173a11a30ebbe198a61f5292e4ca433 68656c6c6f20736572766572
     sock.close()
--- socket_demo/udp_bytes/test.py
+++ socket_demo/udp_bytes/test.py
@@ -1,5 +1,8 @@
 # coding: utf-8
-from socket import *
+
+# a = b'\xde\xad\xbe\xef'
+# 这种形式是python中的bytes的定义方法,跟nodejs,c有所不同
+
 import binascii
 
 if __name__ == "__main__":
@@ -12,19 +15,18 @@ if __name__ == "__main__":
     print(a_bytes)
     print(a_bytearray) #4
     print(binascii.hexlify(a)) #deadbeef
-
     print(len(a)) #4
     print(len(a_bytes)) #4
     print(len(a_bytearray)) #4
 
-    '''
-    c = bytearray()
-    # d =
-    a = bytes(b'haah')
-    b = bytes("你好")
-    print len(a)
-    print len(b)
-    print b
-    print c
-    print len(c)
-    '''
+    b = u'你好'
+    b1 = bytearray(b, encoding='utf-8')
+
+    print(b) #你好
+    print(len(b)) #2
+    print(b1) #你好
+    print(len(b1)) #6
+    print(binascii.hexlify(b1)) #e4bda0e5a5bd
+
+
+
